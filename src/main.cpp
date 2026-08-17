5a14f5182b4c03eef99ac2bdaeb228ca3dcedc29
estelle <cheetos1332@gmail.com>
2026-08-17 20:22:05 +0900

    Explore basic OpenCV Mat properties

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,12 @@
 
 int main() {
     std::cout << "Program started." << std::endl;
+
+    cv::Mat frame(2, 3, CV_8UC3);
+    std::cout << frame.empty() << std::endl;
+    std::cout << frame.channels() << std::endl;
+    std::cout << frame.rows << std::endl;
+    std::cout << frame.cols << std::endl;
+
     return 0;
 }
