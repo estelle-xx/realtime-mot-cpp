0c08551f6982a9859f8578a6ad14a3c3a2f26583
estelle <cheetos1332@gmail.com>
2026-08-20 16:27:53 +0900

    Handle camera open failure

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@ int main() {
 
     std::string pipeline = "icamerasrc buffer-count=7 ! videoconvert ! appsink";
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
-    std::cout << cap.isOpened() << std::endl;
+    
+    if (!cap.isOpened()){
+        std::cerr << "Failed to open camera." << std::endl;
+        return 1;
+    }
 
     cv::Mat frame;
 
@@ -14,7 +18,7 @@ int main() {
         if (!cap.read(frame)){
             break;
         }
-        
+
         cv::imshow("Camera", frame);
 
         int key = cv::waitKey(1);
